74a85428d1f5da78ba65a829d21908bbadc30145
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2021-10-03 16:15:35 -0300

    Modificando codigo

--- src/Library/Characters/Dwarf.cs
+++ src/Library/Characters/Dwarf.cs
@@ -2,56 +2,14 @@ using System.Collections.Generic;
 
 namespace RoleplayGame
 {
-    public class Dwarf : ICharacter
+    public class Dwarf : Hero
     
     {
-        private int health = 100;
-        private List<IItem> items;
 
         public Dwarf(string name)
         {
             this.Name = name;
         }
 
-        public string Name { get; set; }
-
-        public int AttackValue{get;set;}
-
-        public int DefenseValue{get;set;}
-
-        public int Health
-        {
-            get
-            {
-                return this.health;
-            }
-            private set
-            {
-                this.health = value < 0 ? 0 : value;
-            }
-        }
-
-        public void ReceiveAttack(int power)
-        {
-            if (this.DefenseValue < power)
-            {
-                this.Health -= power - this.DefenseValue;
-            }
-        }
-
-        public void Cure()
-        {
-            this.Health = 100;
-        }
-
-        public void AddItem(IItem item)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void RemoveItem(IItem item)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
