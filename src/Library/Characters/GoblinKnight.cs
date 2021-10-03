0a20a8b98c673ce6cf1c528406f6ae20eea5ee1c
Rodrigo Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2021-10-03 17:00:44 -0300

    Goblin Knight

---
+++ src/Library/Characters/GoblinKnight.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace RoleplayGame
+{
+    public class DeathKnight : BadGuy
+    {
+    
+        public DeathKnight(string name)
+        {
+            this.Name = name;
+        }
+
+    }
+}
